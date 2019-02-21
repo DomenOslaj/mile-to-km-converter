692629b3d2edf26031c00b26545411643966a2f1
DomenOslaj <doslaj@gmail.com>
2019-02-21 10:51:35 +0100

    Try if number or string.

--- converter.py
+++ converter.py
@@ -1,16 +1,20 @@
-print "Hallo, that is a unit converter that converts kilometers into miles. "
+print "Hallo, this is a unit converter that converts kilometers into miles. "
 
 while True:
     km = raw_input("Please enter number of kilometers. ")
 
-    km = int(km)
+    try:
+        km = float(km)              #try if number or string
+        miles = km * 0.621371
+        print miles
 
-    miles = km * 0.621371
+        choice = raw_input("Would you like to do another conversion (y/n): ")
 
-    print miles
+        if choice == "n":
+            print "Thank you for using our converter."
+            break
 
-    choice = raw_input("Would you like to do another conversion (y/n): ")
 
-    if choice == "n":
-        print "Thank you for using our converter."
-        break
+    except:          #if string
+        print "Please enter numbers only. "
+
