f399e0657786f3f5005f255f5e3d08258ffabadf
DomenOslaj <doslaj@gmail.com>
2019-02-27 14:09:34 +0100

    Done: - float numbers - comma, dot - if user enter a string is he warned with "Numbers only!"

--- converter.py
+++ converter.py
@@ -1,20 +1,26 @@
-print "Hallo, this is a unit converter that converts kilometers into miles. "
+print "Hallo, this is the unit converter that converts kilometers into miles. "
 
 while True:
     km = raw_input("Please enter number of kilometers. ")
 
     try:
-        km = float(km)              #try if number or string
+        km = float(km.replace(",", "."))              # try if number or string/replace comma with dot
+
         miles = km * 0.621371
+
         print miles
 
         choice = raw_input("Would you like to do another conversion (y/n): ")
 
-        if choice == "n":
+        if choice.lower() == "n":
             print "Thank you for using our converter."
             break
 
+        elif choice.lower() != "n" and choice.lower() != "y":
+            print "I understand Y and N only!"
 
-    except:          #if string
-        print "Please enter numbers only. "
+        else:
+            continue
 
+    except ValueError:         # if string
+        print "Numbers only! "
